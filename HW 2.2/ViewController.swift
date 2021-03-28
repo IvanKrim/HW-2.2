75d3a19883571163fa7d6c0d8c403493d7aca245
JohnKrim <reamonn96@gmail.com>
2021-03-28 13:13:59 +0300

    Final

--- HW 2.2/ViewController.swift
+++ HW 2.2/ViewController.swift
@@ -18,6 +18,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
     
     // MARK: - IB Outlets
@@ -35,35 +37,27 @@ class ViewController: UIViewController {
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
     
-    
+    // MARK: - Private Properties
+    private let textColour = UIColor.white
     
     // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // label Colour в Private Methods !!!
-        redColourLabel.text = "Red:"
-        redColourLabel.font = redColourLabel.font.withSize(17)
-        
-        greenColourLabel.text = "Green:"
-        greenColourLabel.font = greenColourLabel.font.withSize(17)
-        
-        blueColourLabel.text = "Blue:"
-        blueColourLabel.font = blueColourLabel.font.withSize(17)
-        
-    
-        
+        view.backgroundColor = .brown
         displayedColorView.layer.cornerRadius = displayedColorView.frame.height * 0.1
+        
+        addRedColourLabelParamets()
+        addGreenColourLabelParamets()
+        addBlueColourLabelParametrs()
         
         addRedSliderParametrs()
         addGreenSliderParametrs()
         addBlueSliderParametrs()
         
-        
-        
-        
-        
-        
+        addRedSliderValueLabelParametrs()
+        addGreenSliderValueLabelParametrs()
+        addBlueSliderValueLabelParametrs()
     }
     // MARK: - IB Actions
     @IBAction func redSliderAction() {
@@ -82,37 +76,64 @@ class ViewController: UIViewController {
     }
     
     // MARK: - Private Methods
+    private func addRedColourLabelParamets() {
+        redColourLabel.text = "Red:"
+        redColourLabel.font = redColourLabel.font.withSize(17)
+        redColourLabel.textColor = textColour
+    }
+    
+    private func addGreenColourLabelParamets() {
+        greenColourLabel.text = "Green:"
+        greenColourLabel.font = greenColourLabel.font.withSize(17)
+        greenColourLabel.textColor = textColour
+    }
+    
+    private func addBlueColourLabelParametrs() {
+        blueColourLabel.text = "Blue:"
+        blueColourLabel.font = blueColourLabel.font.withSize(17)
+        blueColourLabel.textColor = textColour
+    }
+    
+    private func addRedSliderValueLabelParametrs() {
+        redSliderValueLabel.textColor = textColour
+        redSliderValueLabel.font = redSliderValueLabel.font.withSize(17)
+        redSliderValueLabel.text = String(format: "%.0f", redSlider.value * 255)
+    }
+    
+    private func addGreenSliderValueLabelParametrs() {
+        greenSliderValueLabel.textColor = textColour
+        greenSliderValueLabel.font = greenSliderValueLabel.font.withSize(17)
+        greenSliderValueLabel.text = String(format: "%.0f", greenSlider.value * 255)
+    }
+    
+    private func addBlueSliderValueLabelParametrs() {
+        blueSliderValueLabel.textColor = textColour
+        blueSliderValueLabel.font = blueSliderValueLabel.font.withSize(17)
+        blueSliderValueLabel.text = String(format: "%.0f", blueSlider.value * 255)
+    }
+    
     private func addRedSliderParametrs() {
-        redSlider.value = 0.1
+        redSlider.value = 0.4
         redSlider.minimumValue = 0
         redSlider.maximumValue = 1
         redSlider.minimumTrackTintColor = .systemRed
         redSlider.maximumTrackTintColor = .systemGray3
-        
-        redSliderValueLabel.font = redSliderValueLabel.font.withSize(17)
-        redSliderValueLabel.text = String(redSlider.value)
     }
     
-    private func addGreenSliderParametrs(){
-        greenSlider.value = 0.2
+    private func addGreenSliderParametrs() {
+        greenSlider.value = 0.4
         greenSlider.minimumValue = 0
         greenSlider.maximumValue = 1
         greenSlider.minimumTrackTintColor = .systemGreen
         greenSlider.maximumTrackTintColor = .systemGray3
-        
-        greenSliderValueLabel.font = greenSliderValueLabel.font.withSize(17)
-        greenSliderValueLabel.text = String(greenSlider.value)
     }
     
-    private func addBlueSliderParametrs(){
-        blueSlider.value = 0.3
+    private func addBlueSliderParametrs() {
+        blueSlider.value = 0.4
         blueSlider.minimumValue = 0
         blueSlider.maximumValue = 1
         blueSlider.minimumTrackTintColor = .systemBlue
         blueSlider.maximumTrackTintColor = .systemGray3
-        
-        blueSliderValueLabel.font = blueSliderValueLabel.font.withSize(17)
-        blueSliderValueLabel.text = String(blueSlider.value)
     }
     
     private func changeColor() {
@@ -124,7 +145,5 @@ class ViewController: UIViewController {
         
         displayedColorView.backgroundColor = backgroundColor
     }
-    
-    
 }
 
